cc705e50322f5bca32f2369c9faa102af6f4fbf2
Jefferson Soares <jefferson1104junior@gmail.com>
2024-03-14 12:25:08 -0300

    fix: update SEO configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "SoaresDev",
     description:
-        "Web developer since 2014, specializing in front-end, back-end, and mobile development, leveraging technologies from the JavaScript ecosystem such as React.js, Node.js, React Native, and more."
+        "I am software engineer, almost 10 years of experience specializing in front-end, back-end, and mobile development, expertise with tecnologies such as JavaScript, TypeScript, React.js, React Native, Node.js, Redux, Unit tests, E2E tests, AWS, SQL and No-SQL and many others."
 };
 
 export default function RootLayout({
@@ -18,17 +18,12 @@ export default function RootLayout({
     return (
         <html lang="en" className="scroll-smooth">
             <head>
-                <title>SoaresDev</title>
+                <title>{`${metadata.title}`}</title>
                 <meta name="theme-color" content="#1d1b27" />
+                <meta name="description" content={`${metadata.description}`} />
                 <link rel="shortcut icon" href="/img/icon-512.png" />
                 <link rel="apple-touch-icon" href="/img/icon-512.png" />
                 <link rel="manifest" href="/manifest.json" />
-                <link
-                    rel="icon"
-                    href="/images/icon-192.png"
-                    type="image/png"
-                    sizes="32x32"
-                />
             </head>
             <body className={inter.className}>{children}</body>
         </html>
